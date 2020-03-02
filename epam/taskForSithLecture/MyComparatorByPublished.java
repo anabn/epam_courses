a5ec04a68178e14d5e724c447f7b9afa985569e8
anabn <46251223+anabn@users.noreply.github.com>
2020-03-02 12:46:31 +0200

    Unused classes

--- epam/taskForSithLecture/MyComparatorByPublished.java
+++ epam/taskForSithLecture/MyComparatorByPublished.java
@@ -1,12 +1,12 @@
-package epam.taskForSithLecture;
-
-import java.util.Comparator;
-
-public class MyComparatorByPublished implements Comparator{
-    @Override
-    public int compare(Object o1, Object o2) {
-        String pub1 = ((Book)o1).getPublished();
-        String pub2 = ((Book)o2).getPublished();
-        return pub1.compareTo(pub2);
-    }
-}
+//package epam.taskForSithLecture;
+//
+//import java.util.Comparator;
+//
+//public class MyComparatorByPublished implements Comparator{
+//    @Override
+//    public int compare(Object o1, Object o2) {
+//        String pub1 = ((Book)o1).getPublished();
+//        String pub2 = ((Book)o2).getPublished();
+//        return pub1.compareTo(pub2);
+//    }
+//}
